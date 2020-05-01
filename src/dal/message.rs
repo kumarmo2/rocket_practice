827da422767a252a36dc4548920ca920e8b0a536
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-01 20:50:30 +0530

    generated event on message send.

--- src/dal/message.rs
+++ src/dal/message.rs
@@ -1,23 +1,28 @@
-use crate::models::{Message};
-use crate::dtos::{MessageCreateRequest};
+use crate::dal::last_insert_id;
+use crate::dtos::MessageCreateRequest;
+use crate::models::Message;
 use crate::schema::*;
-use diesel::MysqlConnection;
+use diesel::dsl::select;
+use diesel::insert_into;
 use diesel::prelude::*;
-use diesel::{insert_into};
+use diesel::sql_types::{Integer, Text};
+use diesel::MysqlConnection;
 
-
-pub fn create(dto: &MessageCreateRequest, conn: &MysqlConnection) -> bool {
+// TODO: remove dependency of diesel.
+pub fn create(dto: &MessageCreateRequest, conn: &MysqlConnection) -> Option<i32> {
     use crate::schema::messages::dsl::*;
-    let result = insert_into(messages)
-        .values(dto)
-        .execute(conn);
+
+    let result = insert_into(messages).values(dto).execute(conn);
+
     match result {
         Ok(_) => {
-            true
-        },
+            // TODO: reduce these 2 db calls to a single one.
+            let ids = select(last_insert_id).load(conn).unwrap();
+            return Some(ids[0]);
+        }
         Err(reason) => {
             println!("message insertion failed: {}", reason);
-            false
+            return None;
         }
     }
 }
