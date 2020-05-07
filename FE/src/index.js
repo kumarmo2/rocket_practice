7c9384ced1a42eb8dd3ba763eb8473753d999cfc
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-07 11:50:02 +0530

    adjusted some prettier and eslint configs.

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ThemeProvider, RootRef } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import theme from './theme';
 import App from './App';
 
 render(
-  <Router>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </Router>,
-  document.getElementById('root'),
+  <Provider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
